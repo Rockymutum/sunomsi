40242dbaa2602af8ff2be0b645a386a88f083697
Rocky <rockymutum786@gmail.com>
2025-11-08 01:15:54 +0530

    Update _document.tsx

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -1,9 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="h-full">
       <Head>
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="SUNOMSI" />
+        
         {/* iOS Splash Screens */}
         <link 
           href="/splash.png"
@@ -20,9 +25,19 @@ export default function Document() {
           media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3)"
           rel="apple-touch-startup-image"
         />
-        {/* Add more device sizes as needed */}
+        
+        {/* Prevent phone number detection */}
+        <meta name="format-detection" content="telephone=no" />
+        
+        {/* Theme Color */}
+        <meta name="theme-color" content="#000000" media="(prefers-color-scheme: light)" />
+        <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)" />
+        
+        {/* Preload important resources */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </Head>
-      <body>
+      <body className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white antialiased">
         <Main />
         <NextScript />
       </body>
